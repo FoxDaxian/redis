129f2b213772ef208c94ed11962548e4b15f70cc
a13821190779 <945039036@qq.com>
2018-04-09 12:15:56 +0800

    Update: 写会代码太难了。。。

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
+/*
+ * @Author: fox 
+ * @Date: 2018-04-09 09:35:17 
+ * @Last Modified by: fox
+ * @Last Modified time: 2018-04-09 11:33:37
+ */
+
 const redis = require('redis');
 
 const client = redis.createClient({ host: 'localhost', port: 6379 });
+client.auth(123456, () => {
+	console.log('密码正确');
+})
 
