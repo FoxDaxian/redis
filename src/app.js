a2e6b16c96e5ac11a72e406de15be7ed8a92a375
a13821190779 <945039036@qq.com>
2018-04-12 11:53:05 +0800

    Update: 增加 redis

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@
  * @Author: fox 
  * @Date: 2018-04-09 09:35:17 
  * @Last Modified by: fox
- * @Last Modified time: 2018-04-09 11:33:37
+ * @Last Modified time: 2018-04-12 11:51:54
  */
 
 const redis = require('redis');
+const express = require('express');
+const bluebird = require('bluebird');
+const app = express();
+console.log('启动服务中...');
 
-const client = redis.createClient({ host: 'localhost', port: 6379 });
-client.auth(123456, () => {
-	console.log('密码正确');
-})
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
+const cli = redis.createClient({ host: 'localhost', port: 6379 });
+cli.auth(123456, () => {
+    console.log('密码正确');
+});
+cli.on('error', err => {
+    console.log(`出现错误: ${err}`);
+});
+
+app.get('/', async (req, res) => {
+    const redRes = await cli.hgetallAsync('hash');
+    res.json({
+        name: redRes
+    });
+});
+
+app.listen(3000, () => {
+    console.log('启动服务成功');
+});
